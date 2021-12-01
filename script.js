7025c6756e0d798414e442f347f73c509d34d99c
Kirill <d.str1ke@yandex.ru>
2021-12-01 23:23:46 +0500

    change

--- script.js
+++ script.js
@@ -11,9 +11,11 @@
 //         product.status = 'order';
 //         console.log(product)
 //     }, 2000);
-// }, 2000);         // callback hell
+// }, 2000);         // callback hell хотим избавиться от этого (хотя чет пока все стало сложнее)
 
-const req = new Promise(function(resolve, reset) {
+//  
+
+const req = new Promise(function(resolve, reject) { // создаем константу req с промисом, всегда 2 аргумента resolve and reject
     console.log('Запрос данных');
     setTimeout(() => {
         console.log('Подготовка данных...');
@@ -21,7 +23,7 @@ const req = new Promise(function(resolve, reset) {
             name: 'tv',
             price: 2000
         };
-        resolve(product);
+        resolve(product); // эта функция находится в req.then ниже, она запускается при успешном выполнении функции выше
     }, 2000); 
 });
 
@@ -29,12 +31,36 @@ req.then((product)=>{
     return req2 = new Promise((resolve, reject) => {
         setTimeout(()=> {
             product.status = 'order';
-            resolve(product);
+            resolve(product); // при успешном выполнении второй функции мы прописываем эту функцию для этой функции, т.к. мы использовали сразу return, то мы можем укоротить код, как это сделано ниже
         }, 2000);
     });
-}).then(data => {
+}).then(data => { // вот так
     data.modify = true;
     return data;
-}).then(() => {
-    console.log(data);
+}).then((data) => { // и еще раз
+    console.log(data); // выводим объект products
+}).catch(()=>{
+    console.error('Произошла ошибка'); // вылезет при ошибке, пока не знаю как проверить
+}).finally(() =>{
+    console.log('Finally'); // выполнится в конце промиса, вне зависимости от результата предыдущих шагов
 });
+
+
+function test(time) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    })
+}
+
+
+test(1000).then(() => console.log('1000 ms'));
+test(2000).then(() => console.log('2000 ms'));
+
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('ALL'); 
+// }); // метод для того, чтобы выполнить какие-либо действия после выполнения всех промисов
+
+// Promise.race([test(1000), test(2000)]).then(() => {
+//     console.log('ALL1'); 
+// }); // начинает выполнение когда выполнен первый промис
